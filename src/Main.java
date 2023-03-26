097ab957b896e3d5561edab36ce03321142bbbd3
Постников Максим <mvpost@bk.ru>
2023-03-26 14:33:21 +0300

    Update Main.java
    ашч

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-
         int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        int sum = 0;
-        for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] % 2 == 0) {
-                sum += numbers[i];
-            }
-        }
+
+        int sum = Arrays.stream(numbers)
+                .filter(n -> n % 2 == 0)
+                .sum();
+
         System.out.println("Сумма четных чисел: " + sum);
     }
 }
